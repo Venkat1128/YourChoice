d4adce02550d2940351da75612adc69783c56d6d
Venkat1128 <venkat@giffgaff.co.uk.com>
2017-02-16 15:32:56 +0000

    Implemented logout functionality

---
+++ YourChoice/YourChoiceViewController.swift
@@ -0,0 +1,39 @@
+//
+//  YourChoiceViewController.swift
+//  YourChoice
+//
+//  Created by Venkat Kurapati on 16/02/2017.
+//  Copyright © 2017 Kurapati. All rights reserved.
+//
+
+import UIKit
+
+class YourChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logoutAction(_ sender: Any) {
+        YCDataModel.signOut()
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
